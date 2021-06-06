bc847019cf40158e53efc211bdecc04dcd99f9fa
francescofact <ffattori@me.com>
2021-06-06 20:21:11 +0200

    remove useless code

--- lib/utils.dart
+++ lib/utils.dart
@@ -25,27 +25,3 @@ class SlideRightRoute extends PageRouteBuilder {
         ),
   );
 }
-
-class UpdateText extends StatefulWidget {
-
-  UpdateTextState createState() => UpdateTextState();
-
-}
-
-class UpdateTextState extends State {
-
-  String textHolder = 'Loading...';
-  changeText(String newText) {
-    setState(() {
-      textHolder = newText;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-        '$textHolder',
-        style: TextStyle(fontWeight: FontWeight.bold),
-    );
-  }
-}
